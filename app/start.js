facf98bb370b8e2f35acb177e3b7adae3229d6f1
Florian Mäder <florian@maederbiel.ch>
2021-06-24 16:41:04 +0200

    feat: switch to new client
    Finally!
    
    BREAKING CHANGE: There's no chance this doesn't need action :-D

--- app/start.js
+++ app/start.js
@@ -1,12 +1,14 @@
+// NPM Dependencies
+const Client = require('@rx-irc/client');
+
 // Local Dependencies
-const ClientWrapper = require('./client-wrapper');
 const logger = require('./logger');
 const config = require('../config');
 
-let client = new ClientWrapper(config.client);
+let client = new Client(config.client);
 
 client.connect(() => {
-	logger.info(`Connected to ${config.client.server}.`);
+	logger.info(`Connected to ${config.client.host}.`);
 
 	config.modules
 		.filter(module => module.enabled !== false)
